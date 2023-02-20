b2a19b213dd9676b1b2f19727eabb57a49a82cde
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-02-19 22:56:38 -0300

    feat: create simple combat

--- listeners/webSocket.js
+++ listeners/webSocket.js
@@ -1,13 +1,34 @@
 import { WebSocketServer } from 'ws';
 
+import Hero from '../src/models/Hero.js';
+
 const PORT = 8080;
 
 export const wss = new WebSocketServer({ port: PORT });
 
+const GOBLIN = {
+  name: 'goblin',
+  velocity: 3000,
+  status: {
+    hp: 25,
+    attack: 1,
+    defense: 1,
+  },
+};
+
 wss.on('connection', (ws) => {
-  ws.on('message', (data) => {
-    ws.send(`Received ${data.toString()}`);
-  });
+  setInterval(async () => {
+    const hero = await Hero.findOne();
+    const currentHeroHp = hero.status.hp;
+    const damagedHeroHp = currentHeroHp - GOBLIN.status.attack;
+
+    await Hero.updateOne({}, { 'status.hp': damagedHeroHp });
+    ws.send(
+      `O ${GOBLIN.name} te atacou e desferiu ${
+        GOBLIN.status.attack
+      } de dano. Vida atual: ${JSON.stringify(damagedHeroHp)}`
+    );
+  }, GOBLIN.velocity);
 
   ws.on('error', (err) => ws.send(err));
   ws.send('CONNECTED');
