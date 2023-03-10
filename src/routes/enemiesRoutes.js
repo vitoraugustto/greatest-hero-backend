0dcd006c6f5cee06c8954721c357e98b3827908e
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-03-09 22:21:17 -0300

    feat: create endpoint to post an Enemy

--- src/routes/enemiesRoutes.js
+++ src/routes/enemiesRoutes.js
@@ -1,8 +1,49 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import Enemy from '../models/Enemy.js';
+import cloudinary from '../services/cloudinary.js';
 
 const router = Router();
+const upload = multer({ dest: 'uploads/' });
+
+router.post('/', upload.single('image'), async (req, res) => {
+  const image = req.file;
+  const { name, status } = req.body;
+
+  const enemy = {
+    name,
+    status,
+    image,
+  };
+
+  try {
+    await cloudinary.v2.uploader.upload(
+      image.path,
+      { public_id: image.originalname.split('.')[0] },
+      (_, result) => {
+        enemy.image = result.url;
+      }
+    );
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+
+  if (!name || !image || Object.keys(enemy.status).length === 0) {
+    res.status(422).json({
+      error: "'Name', 'status', e 'image' são obrigatórios.",
+    });
+    return;
+  }
+
+  try {
+    await Enemy.create(enemy);
+
+    res.status(201).json(enemy);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 
 router.get('/', async (_, res) => {
   try {
