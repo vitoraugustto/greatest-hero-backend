0afa719f3252d1fefb9bb160a7751a0ab7d91636
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-03-12 00:00:36 -0300

    feat: remove mock enemy from webSocket

--- listeners/webSocket.js
+++ listeners/webSocket.js
@@ -1,28 +1,30 @@
 import { WebSocketServer } from 'ws';
 
+import Enemy from '../src/models/Enemy.js';
 import Hero from '../src/models/Hero.js';
 
 const PORT = 8080;
 
 export const wss = new WebSocketServer({ port: PORT });
 
-wss.on('connection', (ws) => {
-  const interval = setInterval(async () => {
-    const hero = await Hero.findOne();
-    const heroStatus = { ...hero.status };
+wss.on('connection', async (ws) => {
+  const hero = await Hero.findOne();
+  const enemy = await Enemy.findOne({ name: 'Goblin' });
 
-    heroStatus.hp -= ENEMIES[0].status.attack;
+  const interval = setInterval(async () => {
+    const heroStatus = { ...hero.status };
+    heroStatus.hp -= enemy.status.attack;
 
     await Hero.updateOne({}, { status: heroStatus });
 
     ws.send(
       JSON.stringify({
         hero: { ...hero.toObject(), status: heroStatus },
-        enemy: ENEMIES[0],
-        combat: { damageTaken: ENEMIES[0].status.attack },
+        enemy: enemy,
+        combat: { damageTaken: enemy.status.attack },
       })
     );
-  }, ENEMIES[0].status.attackSpeed);
+  }, enemy.status.attackSpeed);
 
   ws.on('close', async () => {
     clearInterval(interval);
@@ -33,26 +35,3 @@ wss.on('connection', (ws) => {
 
   ws.on('error', (err) => ws.send(err));
 });
-
-const ENEMIES = [
-  {
-    name: 'goblin',
-    status: {
-      hp: 25,
-      maxHp: 25,
-      attack: 3,
-      defense: 1,
-      attackSpeed: 3000,
-    },
-  },
-  {
-    name: 'slime',
-    status: {
-      hp: 12,
-      maxHp: 12,
-      attack: 1,
-      defense: 1,
-      attackSpeed: 2500,
-    },
-  },
-];
