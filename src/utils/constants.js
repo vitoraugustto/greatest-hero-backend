7f164292fe0cb7753bf086c151ccd369fd2892fd
Vitor Augusto <vitoraugusto002@hotmail.com>
2023-07-13 23:52:46 -0300

    style: remove vercel urls

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,9 +6,3 @@ export const CLIENT_URL =
   ENVIRONMENT === 'production'
     ? 'https://greatest-hero.vercel.app'
     : 'http://localhost:5173';
-
-export const VERCEL_URLS = [
-  'https://greatest-hero-vitoraugustto.vercel.app',
-  'https://greatest-hero-35u2tgryy-vitoraugustto.vercel.app',
-  'https://greatest-hero-git-master-vitoraugustto.vercel.app',
-];
